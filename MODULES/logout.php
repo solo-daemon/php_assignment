cb6576021260b29950946b7921d3dd63782b2d09
Amrit <amritdevil1st@gmail.com>
2022-07-16 12:49:23 +0530

    comleted

--- MODULES/logout.php
+++ MODULES/logout.php
@@ -1,10 +1,8 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     if($_POST["action"]=="logout"){
-        $_SESSION=array();
         session_unset();
         session_destroy();
-        $_SESSION['id']==null;
     }
 }
 
